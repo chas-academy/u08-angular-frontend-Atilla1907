9bfd5803677d73d8070f687917f1e60168a8e305
Atilla1907 <atilla.gorgulu@chasacademy.se>
2025-09-30 17:34:42 +0200

    all files and folder

---
+++ my-angular-todo/src/app/components/item-detail/item-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-detail',
+  imports: [],
+  templateUrl: './item-detail.html',
+  styleUrl: './item-detail.scss'
+})
+export class ItemDetail {
+
+}
